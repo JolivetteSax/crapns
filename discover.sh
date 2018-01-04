66d869ba2d3ff26cdb04d9f1d2205727e3dcdedb
esax <erikjsax@gmail.com>
2018-01-04 18:02:44 -0500

    added discovery script for detecting the lan's crapns server... looks for hosts in the lower first couple hundred. Could probably limit further, at the very least getting an address from 200-255 seems unlikeliest

---
+++ discover.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# If the crappy server gets rebooted it might get a different IP from DHCP, this 
+# simple port scan script will look for it. It's possible for a malicious actor
+# to pose as the crapns server for your network, but then youre network security
+# is crappier than this system.
+
+prefix=192.168.1
+pings=1
+
+# I'm not sure how high... usually DHCP gives out lower IP's first, occasionally
+# some DHCP systems might give out random numbers or at least something in 100 range
+for i in $(seq 1 200); do 
+    echo $i; 
+    remote=$prefix.$i
+    ping $remote -c$pings
+    if [ $? -eq 0 ] ; then
+        echo "Remote host $remote is up"
+        # curl operation maxtime 5 seconds, because it's a LAN...
+        result=`curl -f -m 5 http://$remote:10053/discover`
+        if [ $? -eq 0 ] ; then
+            echo "Found server $result at $remote, writing crap.json"
+            echo "{\"crapns\": \"$remote\"}" > crap.json
+            exit 0
+        fi
+    fi
+done
+
